7a675ea54170ce7b1522df76f3469bb66d5ce834
Mitra Ansariola <mansariola@USMAC-D8H03Q.celgene.com>
2021-05-25 14:24:04 -0700

    human data

--- pipeline_exe/scripts/human_cellines/peaks_rnaseq_analysis.R
+++ pipeline_exe/scripts/human_cellines/peaks_rnaseq_analysis.R
@@ -4,6 +4,7 @@
 ##            https://fantom.gsc.riken.jp/5/datafiles/latest/extra/CAGE_peaks/
 
 ####################################################################################################
+library(gtools)
 
 quantile_normalisation <- function(df){
   df_rank <- apply(df,2,rank,ties.method="min")
@@ -32,25 +33,72 @@ eids_cells = EIds[EIds$V2 %in% cellines,]
 rnaseq = rnaseq[,eids_cells$V1]
 colnames(rnaseq) <- eids_cells[which(eids_cells$V1 == colnames(rnaseq), arr.ind = T), "V2"]
 
-asinh.rpkm = as.data.frame(quantile_normalisation(asinh(rnaseq)))
-plot(asinh.rpkm$GM12878, asinh.rpkm$K562)
-plot(asinh.rpkm$GM12878, asinh.rpkm$Universal_Human_Reference)
-plot(asinh.rpkm$K562, asinh.rpkm$Universal_Human_Reference)
-asinh.rpkm$GM12878 = asinh.rpkm$GM12878/asinh.rpkm$Universal_Human_Reference
-asinh.rpkm$K562 = asinh.rpkm$K562/asinh.rpkm$Universal_Human_Reference
+#asinh.rpkm = as.data.frame(quantile_normalisation(asinh(rnaseq)))
+rpkm = rnaseq
+rpkm = log10(rpkm + 0.1)
+#rpkm$fc = foldchange(rpkm$GM12878 + 1, rpkm$K562 + 1)
+#rpkm$log2fc = foldchange2logratio(rpkm$fc, base = 2)
+rpkm$deltaActual = rpkm$K562-rpkm$GM12878
 
-uhrNorm.asinh.rpkm = asinh.rpkm
-plot(uhrNorm.asinh.rpkm$GM12878, uhrNorm.asinh.rpkm$K562)
+a= rpkm
+smoothScatter(a$GM12878, a$K562, cex.axis=2, cex.lab=2, bty="n", xlab="GM12878 expression level (log10)", ylab="K562 expression level (log10)", xlim=c(-1, 4), ylim=c(-1, 4), las=1, cex=.5)
+abline(1,1, col="red")
+abline(-1,1, col="red")
+text(0, 4, labels = paste("Upregulated in K562:", nrow(a[a$deltaActual > 1,])), offset = 0.5, col="black")
+text(3, -1, labels = paste("Upregulated in GM12878:", nrow(a[a$deltaActual < -1,])), offset = 0.5, col="black")
 
-norm.data = uhrNorm.asinh.rpkm
-norm.data = na.omit(norm.data)
-norm.data = norm.data[norm.data$Universal_Human_Reference > 0,]
-norm.data = norm.data[norm.data$GM12878 > 15 | norm.data$K562 > 15,]
-norm.data = norm.data + 0.001
+highlight_df <- rpkm[abs(rpkm$fc) >= 10,]
+
+rpkm %>% 
+  ggplot(aes(x=log10(GM12878),y=log10(K562))) +  
+  geom_point(alpha = 0.2) + theme_bw() +
+  geom_point(data=highlight_df, 
+             aes(x=log10(GM12878),y=log10(K562)),
+             col = "red", size = 3) 
+  
+          
+
+
+                
+rpkm = rpkm[rpkm$Universal_Human_Reference > 0,]
+rpkm$UK562 = rpkm$K562 / rpkm$Universal_Human_Reference
+rpkm$UGM = rpkm$GM12878 / rpkm$Universal_Human_Reference
+head(rpkm)
+#rpkm = rpkm[rpkm$UGM > 0.5 | rpkm$UK562 > 0.5,]
+#rpkm = rpkm[rpkm$GM12878 > 0 | rpkm$K562 > 0,]
+rpkm$log10_GM12878 = log10(rpkm$GM12878 + 0.0001)
+rpkm$log10_K562 = log10(rpkm$K562 + 0.0001)
+rpkm$log_FC = log2((rpkm$GM12878 + 1)/(rpkm$K562 + 1))
+rpkm$log_logFC = log2((rpkm$GM12878 + 1)/(rpkm$K562 + 1))
+plot(rpkm$log10_GM12878, rpkm$log10_K562, xlab = "GM12878  log10(expression)" , ylab = "K562 log10(expression)")
+rpkm$expressed_at <- ifelse(rpkm$log2fc > 2.1, "GM12878", ifelse(rpkm$log2fc < -2.1, "K562", "-1000")) 
+table(rpkm$expressed_at)
+a = rpkm[(rpkm$log10_GM12878 > 0 & rpkm$log10_K562 < 0) | (rpkm$log10_GM12878 < 0 & rpkm$log10_K562 > 0),]
+#plot(asinh.rpkm$GM12878, asinh.rpkm$Universal_Human_Reference)
+#plot(asinh.rpkm$K562, asinh.rpkm$Universal_Human_Reference)
+#asinh.rpkm$GM12878 = asinh.rpkm$GM12878/asinh.rpkm$Universal_Human_Reference
+#asinh.rpkm$K562 = asinh.rpkm$K562/asinh.rpkm$Universal_Human_Reference
+
+#uhrNorm.asinh.rpkm = asinh.rpkm
+#plot(uhrNorm.asinh.rpkm$GM12878, uhrNorm.asinh.rpkm$K562)
+
+#norm.data = uhrNorm.asinh.rpkm
+#norm.data = na.omit(norm.data)
+#norm.data = norm.data[norm.data$Universal_Human_Reference > 0,]
+#norm.data = norm.data[norm.data$GM12878 > 15 | norm.data$K562 > 15,]
+#norm.data = norm.data + 0.001
 
 norm.data$logFC_KG = log2(norm.data$K562 / norm.data$GM12878)
-norm.data$expressed_at <- ifelse(norm.data$logFC_KG > 3, "K562", ifelse(norm.data$logFC_KG < -3, "GM12878", "-1000")) 
+norm.data$expressed_at <- ifelse(norm.data$logFC_KG > 10, "K562", ifelse(norm.data$logFC_KG < -10, "GM12878", "-1000")) 
 table(norm.data$expressed_at)
+
+head(norm.data)
+diff_expr = norm.data
+rsem_header = c("Accession",	"baseMean",	"mean_leaf_norm",	"mean_root_norm",	"foldChange",	"b",	"pval",	"qval")
+diff_expr$Accession = rownames(diff_expr)
+diff_expr$baseMean = diff_expr$Universal_Human_Reference
+diff_expr$Universal_Human_Reference <- NULL
+write.table(diff_expr, file = paste(indir, "/agrawal_human_K562_GM12878_diff_expressed_gennes.txt", sep = ""), sep = "\t", quote = F, row.names = F, col.names = T)
 
 ############
 ## TSS PEAKS
@@ -107,14 +155,16 @@ head(norm.data)
 
 TEP_header = c("Chromosome","Strand","Start","End","ReadCount","ModeLocation","ModeReadCount","Shape","TranscriptLocation","TranscriptID","GeneName","GeneType","X..Capped","tissue")
 cage = merge(cage, gene_table, by.x = "gene_id", by.y = "NCBI.gene.ID")
-cage = cage[,c("Chr", "Strand", "Start", "End", "tpm", "ens.id")] 
+cage = cage[,c("Chr", "Strand", "Start", "End", "tpm", "ens.id", "cell_line")] 
 colnames(cage)[1:5] <- TEP_header[1:5]
-colnames(cage)[6] <- "GeneName"
+colnames(cage)[6:7] <- c("GeneName", "tissue")
+
 cage$ModeLocation <- as.numeric(cage$Start) + abs(as.numeric(cage$End) - as.numeric(cage$Start))/2
 cage$ModeReadCount = cage$ReadCount/2
-head(cage)
 cage$Shape = "None"
 cage$TranscriptLocation = "tss"
 cage$TranscriptID = cage$GeneName
 cage$GeneType = "gene"
 cage$X..Capped = 100
+head(cage)
+write.csv(cage[,TEP_header], file = paste(indir, "/FANTOM5_K562_GM12878_CAGE_TSS_peaks_forTEP.csv", sep = ""), quote = F, row.names = F, col.names = T)
